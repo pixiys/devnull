ae287741024858f7a12c186e7e518913f84f422d
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-08-22 15:42:12 +0200

    Update fun.js

--- commands/fun.js
+++ commands/fun.js
@@ -1,19 +1,23 @@
 {
-  coinflip:{
-	usage: config.prefix+"coinflip",
-	desc: "Flips a coin.",
-	func: function (bot,msg,command,args){
-	 function doRandHT(){
-	 var rand = ['Heads!','Tails!']
-	 
-	 return rand[Math.floor(Math.random()*rand.length)];
-}
-let coinEmbed = new Discord.RichEmbed() 
-	.setDescription("~COINFLIP~")
-	.setColor(config.embedColor)
-	.addField("The winner is "+doRandHT());
-		
-	msg.channel.send(coinEmbed);
+ flip: {
+  usage: config.prefix + "flip",
+  desc: "Flips a coin.",
+  func: function(bot, msg, command, args) {
+   function doRandHT() {
+    var rand = ['Heads!', 'Tails!']
+
+    return rand[Math.floor(Math.random() * rand.length)];
+   }
+   msg.channel.send({
+    embed: {
+     title: "~COINFLIP~",
+     color: config.embedColor,
+     fields: [{
+      name: "The winner is",
+      value: doRandHT()
+     }]
+    }
+   });
   }
- },
+ }
 }
