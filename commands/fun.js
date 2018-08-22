a5002048498aab523d9d5dfe9ed6475d91152fcf
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-08-22 12:27:47 +0200

    Add coinflip command.
    A command that flips a coin and returns the end result ("Heads!" or "Tails!")

--- commands/fun.js
+++ commands/fun.js
@@ -1,1 +1,19 @@
-{}
+{
+  coinflip:{
+	usage: config.prefix+"coinflip",
+	desc: "Flips a coin.",
+	func: function (bot,msg,command,args){
+	 function doRandHT(){
+	 var rand = ['Heads!','Tails!']
+	 
+	 return rand[Math.floor(Math.random()*rand.length)];
+}
+let coinEmbed = new Discord.RichEmbed() 
+	.setDescription("~COINFLIP~")
+	.setColor(config.embedColor)
+	.addField("The winner is "+doRandHT());
+		
+	msg.channel.send(coinEmbed);
+  }
+ },
+}
