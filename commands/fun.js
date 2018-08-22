1b26fa005e07a5e8b329814fe344b2e0b031e9fc
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-08-22 16:11:28 +0200

    Add coinflip command. (#4)
    * Add coinflip command.
    
    A command that flips a coin and returns the end result ("Heads!" or "Tails!")
    
    * Update fun.js
    
    * update 2 of fun.js (coin flip)
    
    All credit to pixiys

--- commands/fun.js
+++ commands/fun.js
@@ -1,1 +1,15 @@
-{}
+{
+ flip: {
+  usage: config.prefix + "flip",
+  desc: "Flips a coin.",
+  func: function(bot, msg, command, args) {
+   msg.channel.send({
+    embed: {
+     title: "Coinflip",
+     color: config.embedColor,
+     description: "The winner is " + ((Math.random() >= 0.5) ? "Heads!" : "Tails!")
+    }
+   });
+  }
+ }
+}
