61a04d1a9b06c6033b18ed6872748e405a1b56d0
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-08-23 12:35:26 +0200

    Added !ask. (#7)
    * Added !ask.
    
    Usage: !ask <question>
    If no question is asked, the bot will tag the user who ran the command asking them to ask a full question.
    
    * Update fun.js
    
    * Clean up / simplifying

--- commands/fun.js
+++ commands/fun.js
@@ -24,5 +24,22 @@
     }
    });
   }
+ },
+ ask: {
+  usage: config.prefix + "ask <question>",
+  desc: "Responds to the question you ask it.",
+  func: function(bot, msg, command, args) {
+   if (!args[1]) return msg.reply("Please ask a full question!");
+   var sayings = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes","Most likely", "Outlook good", "Yes",
+                  "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+                  "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+   msg.channel.send({
+     embed: {
+     title: config.name+" says",
+     color: config.embedColor,
+     description: sayings[Math.floor((Math.random() * sayings.length))]
+    }
+   });
+  }
  }
 }
