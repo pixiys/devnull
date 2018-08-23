25a5fbf0a54115f443d8f78555f328a3d9de4026
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-08-23 12:23:11 +0200

    Clean up / simplifying
    Thanks, pixiys!

--- commands/fun.js
+++ commands/fun.js
@@ -30,16 +30,14 @@
   desc: "Magic 8-ball",
   func: function(bot, msg, command, args) {
 
-   if (!args[2]) return msg.reply("Please ask a full question!");
+   if (!args[1]) return msg.reply("Please ask a full question!");
    var sayings = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
-
-   var result = Math.floor((Math.random() * sayings.length) + 0);
 
    msg.channel.send({
      embed: {
      title: "The Magic 8-ball says",
      color: config.embedColor,
-     description: sayings[result]
+     description: sayings[Math.floor((Math.random() * sayings.length))]
     }
    });
   }
