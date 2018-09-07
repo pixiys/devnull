7cbe6edd4b3b1bc200317dd72870b08d5385b50c
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-09-07 21:01:42 +0200

    Added warn command

--- commands/util.js
+++ commands/util.js
@@ -1,1 +1,22 @@
-{}
+{
+	warn: {
+  usage: config.prefix + "warn <@uid> <reason>",
+  desc: "Warns a specified user with a timestamp",
+  func: function(bot, msg, command, args) {
+   
+   const reason = args.slice(1).join(' ')
+   let member = msg.mentions.members.first()
+   const warning = msg.guild.roles.find("name", "warning")
+   const author = msg.author.id
+   
+   msg.channel.send({
+    embed: {
+     title: "Warning",
+     color: config.embedColor,
+     description: `User ` + member + ` was warned by ` + `<@` + author + `>` + ` for ` + reason + `at` + '`' + msg.createdTimestamp + '`'
+    }
+   });
+   member.addRole(warning)
+  }
+ },
+}
