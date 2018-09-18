2fef06d5edb4649f05f19c5e83f1be427c88846d
HazTheWaz <42550265+HazTheWaz@users.noreply.github.com>
2018-09-18 12:35:50 +0200

    Added warn command (#9)
    * Added warn command
    
    * Update to !warn
    
    updated to include a timestamp and from now it will send the message to a specified channel (in this case a channel named `warnings`)
    
    * Update #2
    
    fixed the warn command to send the message to the warnings channel and added a kick command
    
    * Update #idfk
    
    the commands use the confgi file to find the "warning" role and the "warnings" channel + added a ban command.
    PS. use the permissions.json
    
    * Update #1337
    
    The embedded message will now say `for no reason` If no reason is specified

--- commands/util.js
+++ commands/util.js
@@ -1,1 +1,69 @@
-{}
+{
+	warn: {
+  usage: config.prefix + "warn <@uid> <reason>",
+  desc: "Warns a specified user",
+  func: function(bot, msg, command, args) {
+   
+   let reason = args.slice(1).join(' ')
+   let member = msg.mentions.members.first()
+   let warning = msg.guild.roles.find('name', config.warningRole)
+   let author = msg.author.id
+   let wchannel = client.channels.find('name', config.actionsChannel)
+   
+   if(!reason){reason = "no reason"}
+   
+   wchannel.send({
+    embed: {
+     title: "Warning",
+     color: config.embedColor,
+     description: `User ` + member + ` was warned by ` + `<@` + author + `>` + ` for ` + reason + ` at ` + '`' + msg.createdTimestamp + '`'
+    }
+   });
+   member.addRole(warning)
+  }
+ },
+ kick: {
+  usage: config.prefix + "kick <@uid> <reason>",
+  desc: "Kicks a specified user.",
+  func:function(bot, msg, command, args) {
+		 
+   let member = msg.mentions.members.first()
+   let reason = args.slice(1).join(' ')
+   let author = msg.author.id
+   let wchannel = client.channels.find("name", config.actionsChannel)
+		 
+   if(!reason){reason = "no reason"}
+		 
+   wchannel.send({
+    embed: {
+     title: "Kick",
+     color: config.embedColor,
+     description: `User ` + member + ` was kicked by ` + `<@` + author + `>` + ` for ` + reason + ` at ` + '`' + msg.createdTimestamp + '`'
+    }
+   });
+   member.kick(reason)
+   }
+ },
+ ban: {
+  usage: config.prefix + "ban <@uid> <reason>",
+  desc: "Bans a specified user.",
+  func: function(bot, msg, command, args) {
+		 
+   let member = msg.mentions.members.first()
+   let reason = args.slice(1).join(' ')
+   let author = msg.author.id
+   let wchannel = client.channels.find("name", config.actionsChannel)
+		 
+   if(!reason){reason = "no reason"}
+		 
+   wchannel.send({
+    embed: {
+     title: "BAN HAMMER",
+     color: config.embedColor,
+     description: `User ` + member + ` was banned by ` + `<@` + author + `>` + ` for ` + reason + ` at ` + '`' + msg.createdTimestamp + '`'
+    }
+   });
+   member.ban(reason)
+  }
+ }
+}
